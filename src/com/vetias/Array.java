10e99a9d59a055b9be956f3a4191a55993b976e3
sachin prabhu <sachinprabhup24cse@vetias.ac.in>
2025-07-02 14:46:05 +0530

    Array Example

---
+++ src/com/vetias/Array.java
@@ -0,0 +1,8 @@
+public class Array{
+    public static void main(String[] args) {
+        int[] marks = {97,89,95,94,85};
+        for (int mark:marks) {
+            System.out.println(mark);
+        }
+    }    
+}
