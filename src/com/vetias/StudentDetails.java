7d4774ee3427a513db07f7d90e6e55da411f1bfd
sachin prabhu <sachinprabhup24cse@vetias.ac.in>
2025-07-02 20:45:20 +0530

    Student details with rollnumber.

---
+++ src/com/vetias/StudentDetails.java
@@ -0,0 +1,18 @@
+public class StudentDetails {
+    public static void main(String[] args) {
+        String[][] students = {
+            {"2422K1311", "Sachin"},
+            {"2422K1312", "Prabhu"},
+            {"2422K1313", "James"},
+            {"2422K1314", "Ravi"},
+            {"2422K1315", "Suresh"}
+        };    
+
+        System.out.println("Student Details:");
+        for (int i = 0; i < students.length; i++) {
+            String rollNo = students[i][0];
+            String name = students[i][1];
+            System.out.printf("Roll No: "+rollNo+",Name: "+name+"%n");
+        }
+    }
+}
