953f8aa57611fa6c136a0f1ad1913782dab4bdf5
sachinprabhu <sachinprabhup24cse@vetias.ac.in>
2025-07-04 10:38:51 +0530

    Changes the output form.

--- src/com/vetias/workshop/basics/Employee.java
+++ src/com/vetias/workshop/basics/Employee.java
@@ -1,23 +1,23 @@
 public class Employee {
     public static void main(String[] args) {
-        String[][] employee= new String[5][3];
-        employee[0][0]="E0001";
-        employee[0][1]="Sachin";
-        employee[0][2]="35000";
-        employee[1][0]="E0002";
-        employee[1][1]="Shobha";
-        employee[1][2]="39000";
-        employee[2][0]="E0003";
-        employee[2][1]="Rahul";
-        employee[2][2]="45000";
-        employee[3][0]="E0004";
-        employee[3][1]="Rahul";
-        employee[3][2]="50000";
-        employee[4][0]="E0005";
-        employee[4][1]="Suresh";
-        employee[4][2]="59000";
-        for (int i = 0; i < employee.length; i++) {
-            System.out.println("Employee ID: "+employee[i][0]+", Name: "+employee[i][1]+", Salary: "+employee[i][2]);
+        String[][] employees= new String[5][3];
+        employees[0][0]="E0001";
+        employees[0][1]="Sachin";
+        employees[0][2]="35000";
+        employees[1][0]="E0002";
+        employees[1][1]="Shobha";
+        employees[1][2]="39000";
+        employees[2][0]="E0003";
+        employees[2][1]="Rahul";
+        employees[2][2]="45000";
+        employees[3][0]="E0004";
+        employees[3][1]="Rahul";
+        employees[3][2]="50000";
+        employees[4][0]="E0005";
+        employees[4][1]="Suresh";
+        employees[4][2]="59000";
+        for (String[] employee : employees) {
+            System.out.println(Arrays.toString(employee));
         }
     }
 }
