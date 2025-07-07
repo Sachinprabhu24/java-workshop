a4698b8567baa5d1eef24182a1c3c6daf9c6ccbe
sachinprabhu <sachinprabhup24cse@vetias.ac.in>
2025-07-07 14:23:16 +0530

    String values are joined .

---
+++ src/com/vetias/workshop/basics/StringValueExample.java
@@ -0,0 +1,10 @@
+public class StringValueExample {
+    public static void main(String[] args) { 
+        StringBuilder sd= new StringBuilder();
+        sd.append("Java").append(" is").append(" the").append(" most").append(" used").append(" Language").append(" for").append(" Enterprice").append(" Application");
+        String result=sd.toString();
+        System.out.println(result);
+
+    }
+
+}
