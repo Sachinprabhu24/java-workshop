1e78f7802779e7f39a7e5ac8d3e00622b9b39259
sachinprabhu <sachinprabhup24cse@vetias.ac.in>
2025-07-07 11:05:36 +0530

    Printing words in Array fromat.

--- src/com/vetias/workshop/basics/MultipleStrings.java
+++ src/com/vetias/workshop/basics/MultipleStrings.java
@@ -1,11 +1,10 @@
+import java.util.Arrays;
 
 public class MultipleStrings {
     public static void main(String[] args) {
         String sentence="This program shows how can we split a string into multiple Strings.";
         String[] words = sentence.split(" ");
         System.out.println("The number of words in the sentence is: " + words.length);
-        for (String word : words) {
-            System.out.println(word);
-        }
+        System.out.println(Arrays.toString(words));
     }
 }
