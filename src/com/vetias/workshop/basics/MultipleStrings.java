91ea6f3a0e03343c58a0095205a7b912b563007f
sachinprabhu <sachinprabhup24cse@vetias.ac.in>
2025-07-07 10:58:57 +0530

    Printing each  words line by line.

--- src/com/vetias/workshop/basics/MultipleStrings.java
+++ src/com/vetias/workshop/basics/MultipleStrings.java
@@ -1,7 +1,11 @@
+
 public class MultipleStrings {
     public static void main(String[] args) {
         String sentence="This program shows how can we split a string into multiple Strings.";
         String[] words = sentence.split(" ");
         System.out.println("The number of words in the sentence is: " + words.length);
+        for (String word : words) {
+            System.out.println(word);
+        }
     }
 }
