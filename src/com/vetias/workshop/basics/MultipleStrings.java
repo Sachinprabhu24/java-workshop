d2345b7942d3d8aa1c074aedaf98e9b20564997c
sachinprabhu <sachinprabhup24cse@vetias.ac.in>
2025-07-07 10:40:10 +0530

    Multiple String.

---
+++ src/com/vetias/workshop/basics/MultipleStrings.java
@@ -0,0 +1,7 @@
+public class MultipleStrings {
+    public static void main(String[] args) {
+        String sentence="This program shows how can we split a string into multiple Strings.";
+        String[] words = sentence.split(" ");
+        System.out.println("The number of words in the sentence is: " + words.length);
+    }
+}
