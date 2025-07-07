93afae2a90bdc975af239a2a87f2f15a35a67ea3
sachinprabhu <sachinprabhup24cse@vetias.ac.in>
2025-07-07 11:36:11 +0530

    Printing the sentence and letters in program.

--- src/com/vetias/workshop/basics/MultipleStrings.java
+++ src/com/vetias/workshop/basics/MultipleStrings.java
@@ -1,10 +1,12 @@
-import java.util.Arrays;
 
 public class MultipleStrings {
     public static void main(String[] args) {
-        String sentence="This program shows how can we split a string into multiple Strings.";
+        String sentence="This program shows. how can we split a string. into multiple Strings.";
         String[] words = sentence.split(" ");
+        String[] words2 = sentence.split("\\.");
+        String[] words3 = sentence.split("");
         System.out.println("The number of words in the sentence is: " + words.length);
-        System.out.println(Arrays.toString(words));
+        System.out.println("The number of sentences in the paragraph is: " + words2.length);
+        System.out.println("The number of characters in the sentence is: " + words3.length);
     }
 }
