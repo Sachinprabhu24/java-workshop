df08e4f87f6dd8fc9b52c8b944d305ac56660eed
sachinprabhu <sachinprabhup24cse@vetias.ac.in>
2025-07-07 12:04:01 +0530

    no changes

--- src/com/vetias/workshop/basics/MultipleStrings.java
+++ src/com/vetias/workshop/basics/MultipleStrings.java
@@ -4,9 +4,9 @@ public class MultipleStrings {
         String sentence="This program shows. how can we split a string. into multiple Strings.";
         String[] words = sentence.split(" ");
         String[] words2 = sentence.split("\\.");
-        String[] words3 = sentence.split("");
+        String[] word3 = sentence.split("");
         System.out.println("The number of words in the sentence is: " + words.length);
         System.out.println("The number of sentences in the paragraph is: " + words2.length);
-        System.out.println("The number of characters in the sentence is: " + words3.length);
+        System.out.println("The number of characters in the sentence is: " + word3.length);
     }
 }
