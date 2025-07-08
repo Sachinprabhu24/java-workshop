cf24f882ba62c9910b6f059afe187ff18e822206
sachinprabhu <sachinprabhup24cse@vetias.ac.in>
2025-07-08 10:13:41 +0530

    Printing the age by giving full details of birth .

--- src/com/vetias/workshop/basics/AgeCalculator.java
+++ src/com/vetias/workshop/basics/AgeCalculator.java
@@ -4,10 +4,11 @@ import java.util.Scanner;
 public class AgeCalculator {
     public static void main(String[]args){
         Scanner inputscanner=new Scanner(System.in);
-        System.out.println("Enter your Birth year:");
-        int birthyear=inputscanner.nextInt();
+        System.out.println("Enter your Birth year(YYYY-MM-DD):");
+        String birthdate=inputscanner.nextLine();
+        LocalDate YearBirth= LocalDate.parse(birthdate);
+        int YearOfBirth=YearBirth.getYear();
         int currentyear=LocalDate.now().getYear();
-        int age=currentyear-birthyear;
-        System.out.println("Your age is: " + age);
+        System.out.println("Your age is: " + (currentyear-YearOfBirth));
     }
 }
