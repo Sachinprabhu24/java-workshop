953f9ca0ae4aa194b57efe72d17da428c162dc82
sachinprabhu <sachinprabhup24cse@vetias.ac.in>
2025-07-08 14:17:17 +0530

    Modified.

--- src/com/vetias/workshop/basics/NameGenerated.java
+++ src/com/vetias/workshop/basics/NameGenerated.java
@@ -3,7 +3,7 @@
 import java.util.Scanner;
 
 public class NameGenerated{
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter Your Name: ");
         String Name = scanner.nextLine();
@@ -19,8 +19,7 @@ public class NameGenerated{
         } else if ("Female".equalsIgnoreCase(gender)) {
             prefix = "Mrs.";
         } else {
-            System.out.println("Invalid Gender");
-            throw new IllegalArgumentException("Invalid Gender");
+            throw new Exception("Invalid Gender");
         }
         sb.append(prefix).append(Name).append(", ").append(qualification);
         System.out.println(sb.toString());
