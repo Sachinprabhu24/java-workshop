b74896042afad8725e9aebf7b5dce4ed47ba3174
sachinprabhu <sachinprabhup24cse@vetias.ac.in>
2025-07-08 11:50:13 +0530

    Date Format.

---
+++ src/com/vetias/workshop/basics/DateFormat.java
@@ -0,0 +1,9 @@
+import java.time.LocalDateTime;
+
+public class DateFormat {
+    public static void main(String[] args) {
+        LocalDateTime currentDateTime = LocalDateTime.now();
+        String formattedDate = currentDateTime.toString();
+        System.out.println("Current Date and Time in ISO-8601 format: " + formattedDate);
+    }
+}
