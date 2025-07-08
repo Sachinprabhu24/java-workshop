a179f8a5f1f195f5af9d24c954b2073a7cc71058
sachinprabhu <sachinprabhup24cse@vetias.ac.in>
2025-07-08 14:53:15 +0530

    Exceptional handling example.

---
+++ src/com/vetias/workshop/basics/Calculator.java
@@ -0,0 +1,20 @@
+public class Calculator {
+    public static int divide(int a,int b){
+        try {
+            if(b==0){
+                throw new ArithmeticException("Division by zero is no valid.");
+            }
+        } catch (ArithmeticException e) {
+            System.out.println(e.getMessage());
+        }
+        return a/b;
+        
+    }
+    public static void main(String[] args) {
+        int a=5,b=0;
+        System.out.println("Addition:"+(a+b));
+        System.out.println("Subraction:"+(a-b));
+        System.out.println("Multiplication:"+(a*b));
+        System.out.println("Division:"+(a/b));
+    }
+}
