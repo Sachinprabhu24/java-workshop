bb763eea8ce6d358d6741d025cce6f8e68802604
sachinprabhu <sachinprabhup24cse@vetias.ac.in>
2025-07-09 11:35:32 +0530

    File sharing by binary values.

---
+++ src/com/vetias/workshop/standardlibrary/FilecopyUsingBinary.java
@@ -0,0 +1,21 @@
+package com.vetias.workshop.standardlibrary;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class FilecopyUsingBinary {
+    public static void main(String[] args) {
+        try(FileInputStream InputStream=new FileInputStream("nature.jpg");FileOutputStream OutputStream=new FileOutputStream("outputtt.jpg") ){
+            byte[] chunk = new byte[1024];
+            int byteRead;
+            while((byteRead= InputStream.read(chunk))!=-1){
+                
+                OutputStream.write(chunk, 0, byteRead);
+            }
+
+        }catch(IOException e){
+            System.err.println("Error reading or writing the file.");
+        }
+    }
+}
