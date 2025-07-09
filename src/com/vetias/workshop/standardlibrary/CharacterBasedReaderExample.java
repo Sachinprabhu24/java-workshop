26d6add8a379a33a5e5b67d9f2cb04aaa834f543
sachinprabhu <sachinprabhup24cse@vetias.ac.in>
2025-07-09 10:28:04 +0530

    Character based example.

---
+++ src/com/vetias/workshop/standardlibrary/CharacterBasedReaderExample.java
@@ -0,0 +1,21 @@
+package com.vetias.workshop.standardlibrary;
+
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class CharacterBasedReaderExample {
+    public static void main(String[] args) {
+        try (BufferedReader reader = new BufferedReader(new FileReader("README.md"))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line);
+            }
+        } catch (IOException e) {
+            System.err.println("Error reading file:");
+            e.printStackTrace();
+        }
+
+    }
+}
