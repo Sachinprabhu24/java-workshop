ccbe8b1bf07d3ff2e4ed0347418f36d2220a3289
sachinprabhu24 <sachinprabhup24cse@vetias.ac.in>
2025-07-14 09:26:14 +0530

    Listing Stdents Marks

---
+++ src/main/java/com/vetias/java/workshop/streams/ClassMarks.java
@@ -0,0 +1,10 @@
+package com.vetias.java.workshop.streams;
+import java.util.Arrays;
+import java.util.List;
+
+public class ClassMarks {
+    public static void main(String[] args) {
+        List<Integer> Marks=Arrays.asList(23 ,45 ,90 ,98 ,99 ,100 ,87 ,94);
+        
+    }
+}
