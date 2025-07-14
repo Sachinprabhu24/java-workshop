6d1026fcf25a36eec0c3a1b2bcb97e6fed0a7b15
sachinprabhu24 <sachinprabhup24cse@vetias.ac.in>
2025-07-14 10:52:32 +0530

    modified line

--- src/main/java/com/vetias/java/workshop/tempdata/beans/Organization.java
+++ src/main/java/com/vetias/java/workshop/tempdata/beans/Organization.java
@@ -3,11 +3,11 @@ package com.vetias.java.workshop.tempdata.beans;
 import java.time.LocalDate;
 
 public record Organization(String Name,
-String Website,
-String CompleteAddress,
-String contactNumber,
-String Email,
-long RegistrationNumber,
-LocalDate RegistrationDate) {
+    String Website,
+    String CompleteAddress,
+    String contactNumber,
+    String Email,
+    long RegistrationNumber,
+    LocalDate RegistrationDate) {
 
 }
