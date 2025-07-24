18422ab5e8992e49b9e4da1e39835e3fe9cfaebb
sachinprabhu24 <sachinprabhup24cse@vetias.ac.in>
2025-07-24 21:36:48 +0530

    Add FidIPAddress class to retrieve and display local IP address

---
+++ src/main/java/com/vetias/java/workshop/ipaddress/FidIPAddress.java
@@ -0,0 +1,13 @@
+package com.vetias.java.workshop.ipaddress;
+import java.net.InetAddress;
+
+public class FidIPAddress {
+    public static void main(String[] args) {
+        try {
+            InetAddress ipaddress = InetAddress.getLocalHost();
+            System.out.println("IP Address: " + ipaddress.getHostAddress());
+        } catch (java.net.UnknownHostException e) {
+            System.err.println("Error: " + e.getMessage());
+        }
+    }
+}
